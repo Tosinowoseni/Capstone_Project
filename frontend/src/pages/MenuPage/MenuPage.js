59c185763e21b0d516ded8786a29acae34f5eab2
tosinowoseni <tosinowosen1@icloud.com>
2022-05-10 17:20:51 -0400

    Made Modifications to Web Page

--- frontend/src/pages/MenuPage/MenuPage.js
+++ frontend/src/pages/MenuPage/MenuPage.js
@@ -1,17 +1,8 @@
 import React from "react";
 import products from '../../images/download (1).jpg'
 
-
 const MenuPage = (props) => {
-const MenuPage = (props) => {
-    return (
-        <div className="menupage">
-        <div className="image-wrapper2"><img src={products} width={100} alt="" /></div>
-        
-        </div>
-    )
-}
-    return(
+      return(
         <h1>MenuPage</h1>
         
 
---
+++ frontend/src/pages/ProductPage/ProductPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import productpage from './image/download (1).jpg'
+
+const productpage = () => {
+     return(
+         <h1>productpage</h1>
+    
+    
+    
+         )
+}
+export default productpage
