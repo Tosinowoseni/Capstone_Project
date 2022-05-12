72d5293576fb3b40f5da8b53848cd34ffdb95f5d
tosinowoseni <tosinowosen1@icloud.com>
2022-05-12 14:18:59 -0400

    Added More Images to Product Page and Listed the Prices and Discription

--- frontend/src/pages/ProductPage/ProductPage.js
+++ frontend/src/pages/ProductPage/ProductPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import BodyCream from './image/BodyCream.jpg'
 import Facebalm from './image/Facebalm.jpg'
+import Blacksoap from './image/Blacksoap.jpg'
+import Facecleanser from './image/Facecleanser.jpg'
 
 const ProductPage = () => {
     return (
@@ -20,7 +22,22 @@ const ProductPage = () => {
         <div>
             <p>$35.00</p>
         </div>
+        <img src={Blacksoap} height={400} width={400} alt="Blacksoap" />
+        <div>
+            <h3>Black Soap</h3>
         </div>
+        <div>
+            <p>$40.00</p>
+        </div>
+        <img src={Facecleanser} height={400} width={400} alt="Facecleanser" />
+        <div>
+            <h3>Face Cleanser</h3>
+        </div>
+        <div>
+            <p>$35.00</p>
+        </div>
+        </div>
+    
     );
 };
 
